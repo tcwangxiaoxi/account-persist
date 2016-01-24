19ff023c2f67100ef6e45561be4dc00c014caa8e
tcwangxiaoxi <tcwangxiaoxi@gmail.com>
2016-01-25 01:01:24 +0800

    首次提交到本地仓库

---
+++ src/main/java/com/juvenxu/mvnbook/account/persist/AccountPersistException.java
@@ -0,0 +1,10 @@
+package com.juvenxu.mvnbook.account.persist;
+
+public class AccountPersistException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1360549327007214727L;
+
+}
